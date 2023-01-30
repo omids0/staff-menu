61fe2767ebb1407e29412590e8d9b5ccd3de8329
Omid SoheilNia <soheilniao@gmail.com>
2023-01-30 12:12:05 +0330

    redesign foods card

--- frontend/src/component/HomePage/CatFoodDetails.js
+++ frontend/src/component/HomePage/CatFoodDetails.js
@@ -11,14 +11,14 @@ function CatFoodDetails({ food }) {
   const dispatch = useDispatch();
 
   return (
-    <div className="flex flex-row justify-between my-2 min-h-[7rem] ">
-      <div className="p-4 rounded-r-lg bg-gradient-to-r from-slate-100 to-red-50 w-full flex flex-col justify-between">
-        <p className="text-sm font-bold text-gray-800 flex-wrap">{food.name}</p>
+    <div className="flex md:flex-col justify-between my-2 mx-2 md:min-h-[12rem] w-full md:min-w-[12rem] md:max-w-[12rem] bg-gradient-to-r from-stone-100 to-red-50 shadow-md rounded-md">
+      <div className="p-4  w-full flex flex-col justify-between">
+        <p className="text-sm font-bold text-gray-500 flex-wrap">{food.name}</p>
         <p className="mt-2 text-gray-400 text-sm">
           {food.price.toLocaleString("fa-IR")} تومان
         </p>
       </div>
-      <div className="bg-gray-100 text-white p-4 flex flex-col justify-center items-center sm:min-w-[9rem] min-w-[8rem] rounded-l-lg">
+      <div className=" text-white p-4 flex flex-col justify-center items-center sm:min-w-[9rem] min-w-[8rem] ">
         {foodItem?.qty > 0 ? (
           <div className="flex flex-row text-2xl w-full justify-between">
             <button
@@ -29,7 +29,7 @@ function CatFoodDetails({ food }) {
             >
               +
             </button>
-            <p className="mx-2 text-gray-900">
+            <p className="mx-2 text-gray-600">
               {getThreeDigitNum(foodItem.qty)}
             </p>
             <button
